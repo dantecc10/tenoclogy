738b396797bef68b26a1c036c5b88aea83ae1c5e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-12-28 13:37:04 -0600

    ..

--- assets/docs/papers/research.php
+++ assets/docs/papers/research.php
@@ -1,25 +1,25 @@
 <?php
-	if (isset($_GET['id']) {
-		$url = "";
-		switch ($_GET['id']) {
-			case 1:
-				$url = "Diseño de Interfaz de Bajo Costo para el Desarrollo de Aprendizaje  de Robótica en Niños de Educación Primaria.pdf";
-				break;
-			case 2:
-				$url = "Diseño de Sistema Domótico con Visión Artificial para el Reconocimiento de Lengua de Señas Mexicanas.pdf";
-				break;
-			case 3:
-				$url = "Simulación de los Movimientos de un Robot Flexible Utilizando Actuadores Piezoeléctricos.pdf";
-				break;
-			default:
-				header("Location: ../../");
-				exit;
-				break;
-			
-			header("Location: " . $url . ")";
+if (isset($_GET['id'])) {
+	$url = "";
+	switch ($_GET['id']) {
+		case 1:
+			$url = "Diseño de Interfaz de Bajo Costo para el Desarrollo de Aprendizaje  de Robótica en Niños de Educación Primaria.pdf";
+			break;
+		case 2:
+			$url = "Diseño de Sistema Domótico con Visión Artificial para el Reconocimiento de Lengua de Señas Mexicanas.pdf";
+			break;
+		case 3:
+			$url = "Simulación de los Movimientos de un Robot Flexible Utilizando Actuadores Piezoeléctricos.pdf";
+			break;
+		default:
+			header("Location: ../../");
 			exit;
-		}
-	} else {
-		header("Location: " . $url . ")";
-		exit;
+			break;
+
+			header("Location: " . $url . ")");
+			exit;
 	}
+} else {
+	header("Location: " . $url . ")");
+	exit;
+}
